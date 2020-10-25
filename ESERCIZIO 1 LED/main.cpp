2ad2b7ebbb839313f76078c1a600de1b187efe9b
Giuseppe Riccio <giuseppe.riccio9@studenti.unina.it>
2020-10-25 18:58:59 +0100

    Esercizio LMM

---
+++ ESERCIZIO 1 LED/main.cpp
@@ -0,0 +1,20 @@
+#define RCCENAHB (unsigned int*) 0x40021014;
+#define GPIOEMODER (unsigned int*) 0x48001000;
+#define GPIOEODR (unsigned int*) 0x48001014;
+
+unsigned int *puntatore;
+
+int main()
+{
+
+  puntatore = RCCENAHB;       //RCC AHB enable register (pag. 42)
+  *puntatore |= 1<<21;        //per cambiare solo il bit d'interesse
+  puntatore = GPIOEMODER;     //Registro base GPIOE (pag. 42)
+  *puntatore |= 1<<16;        //per cambiare solo il bit d'interesse
+  puntatore = GPIOEODR;       //Registro GPIO Output Data Register
+  *puntatore |= 1<<8;         //per cambiare solo il bit d'interesse
+
+  while(1);
+  return 0;
+ 
+}
